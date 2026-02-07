976adf9fc4765e706a16567ab3a0a61a4208b57f
Disguised Coffee <68920714+Disguised-Coffee@users.noreply.github.com>
2026-02-07 11:36:07 -0600

    added web export, and vite application

---
+++ site/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <iframe
+        src="godot/godot-game.html"
+        style={{ width: "100%", height: "600px", border: "none" }}></iframe>
+
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
