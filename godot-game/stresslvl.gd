19cc8d7c8999452327c0329694ed052b43b5b8d0
ecalv <eacalvo24@gmail.com>
2026-02-07 13:35:28 -0600

    Added Stress bar *BASE*

---
+++ godot-game/stresslvl.gd
@@ -0,0 +1,12 @@
+extends ProgressBar
+
+
+# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	value = 0.0 # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (value < 100.0):
+		value += 1 * delta
