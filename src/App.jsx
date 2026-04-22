ce0fe2a0f40f49cebfda978b9d8463e8bde648b0
Disguised Coffee <68920714+Disguised-Coffee@users.noreply.github.com>
2026-04-22 17:06:42 -0500

    addition for github page deployment

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function NotFound() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
